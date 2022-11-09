9cdf5ecb5fc28c3318f537ef1f6d436f94e7af90
berrgrayson <gitgrayson@gmail.com>
2022-11-09 13:09:18 -0800

    0x0B. C - malloc, free projects

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes he character c to stdout
+ * @c: The caracter to print
+ *
+ * return: On success1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
